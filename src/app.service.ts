34d43f18726179d31dfd2bf875f418b01910ddf2
mpramoul <mpramoul@gmail.com>
2025-11-14 10:28:29 -0500

    api-gateway, register y login con encriptacion y token ok

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Laravest Mpramoul!';
+    return 'Hello World Microservices!';
   }
 }
---
+++ src/users/auth/auth.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, HttpException, HttpStatus, Inject, Post } from "@nestjs/common";
+import { AuthLoginDto } from "../dto/auth.login.dto";
+import { ClientProxy } from "@nestjs/microservices";
+import { firstValueFrom } from "rxjs";
+import { CreateUserDto } from "../dto/create-user.dto";
+
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject("AUTH_SERVICE") private readonly clientProxy: ClientProxy) {}
+
+  @Post('login')
+  async login(@Body() authLoginDto: AuthLoginDto) {
+    try {
+      const auth_login = await firstValueFrom(this.clientProxy.send({cmd:'login'}, authLoginDto));
+      return auth_login;
+    } catch (error) {
+      console.log("CATCH", error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    try {
+    const auth_register = await firstValueFrom(this.clientProxy.send({cmd:'register'}, createUserDto));
+    return auth_register;
+    } catch (error) {
+      console.log('CATCH', error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors,
+      }, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+}
