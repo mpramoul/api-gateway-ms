402d69df8fba059dbc5135c8be3244022d81b23b
mpramoul <mpramoul@gmail.com>
2025-11-18 08:39:13 -0500

    microdev:: interceptor create_user runing

--- src/users/dto/auth-login.dto.ts
+++ src/users/dto/auth-login.dto.ts
@@ -1,18 +1,6 @@
 import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
-export class CreateUserDto {
-  @IsString()
-  @MaxLength(250,{message:'El nombre debe ser menor o igual a 250 caracteres'})
-  name:string;
-
-  @IsString()
-  @MaxLength(250,{message:'El apellido debe ser menor o igual a 250 caracteres'})
-  surname:string;
-
-  @IsString()
-  @MaxLength(35,{message:'El role debe ser menor o igual a 35 caracteres'})
-  role:string;
-
+export class AuthLoginDto{
   @IsEmail()
   @MinLength(6,{message:'El correo electronico debe ser mayor o igual a 6 caracteres'})
   @MaxLength(250,{message:'El correo electronico debe ser menor o igual a 250 caracteres'})
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { REQUEST } from '@nestjs/core';
 
 @Controller('users')
 export class UsersController {
+  
+  constructor(
+    @Inject('AUTH_SERVICE')
+    private readonly clientProxy: ClientProxy,
+    @Inject(REQUEST) private readonly request,
+    ) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return 'UserController_API_GATEWAY';
-    //return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    try {
+      const authHeader = await this.request.headers['authorization'];
+      const token = typeof authHeader==='string' ? authHeader.replace('Bearer ', '') : undefined;
+      const register_user = await firstValueFrom(this.clientProxy.send({cmd:'register_user'}, {_token: token, ...createUserDto}));
+      return register_user;
+    } catch (error) {
+      console.log('CATCH', error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get()
