70447b0ce7e2507fa75a15602bf033e57109ab54
mpramoul <mpramoul@gmail.com>
2025-11-18 11:51:49 -0500

    microdev:: Microservicio Usuario operativo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -31,22 +31,66 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    //return this.usersService.findAll();
+  async findAll() {
+    try {
+      const authHeader = await this.request.headers['authorization'];
+      const token = typeof authHeader==='string' ? authHeader.replace('Bearer ', '') : undefined;
+      const user_list = await firstValueFrom(this.clientProxy.send({cmd:'user_list'}, {_token: token}));
+      return user_list;
+    } catch (error) {
+      console.log('CATCH', error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    //return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    try {
+      const authHeader = await this.request.headers['authorization'];
+      const token = typeof authHeader==='string' ? authHeader.replace('Bearer ', '') : undefined;
+      const user_list_one = await firstValueFrom(this.clientProxy.send({cmd:'user_list_one'}, {_token: token, user_id: id}));
+      return user_list_one;
+    } catch (error) {
+      console.log('CATCH', error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    //return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    try {
+      const authHeader = await this.request.headers['authorization'];
+      const token = typeof authHeader==='string' ? authHeader.replace('Bearer ', '') : undefined;
+      const user_update = await firstValueFrom(this.clientProxy.send({cmd:'user_update'}, {_token: token, user_id: id, ...updateUserDto}));
+      return user_update;
+    } catch (error) {
+      console.log('CATCH', error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    //return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+      try {
+      const authHeader = await this.request.headers['authorization'];
+      const token = typeof authHeader==='string' ? authHeader.replace('Bearer ', '') : undefined;
+      const user_delete = await firstValueFrom(this.clientProxy.send({cmd:'user_delete'}, {_token: token, user_id: id}));
+      return user_delete;
+    } catch (error) {
+      console.log('CATCH', error);
+      throw new HttpException({
+        message: error.message,
+        errors: error.errors,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
